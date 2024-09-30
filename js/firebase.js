61341e75e2b9b0b360b68af60ee2a82e8d1e2b2c
Chadgamer2023 <Chadgamer938>
2024-09-30 09:17:43 +0100

    Finished

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,13 @@
+// Firebase configuration and initialization
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-};
+    apiKey: "AIzaSyDViywB4Oyq6GF1alYLqzeDjeSz-P8iDYo",
+    authDomain: "messagestorage7346562.firebaseapp.com",
+    databaseURL: "https://messagestorage7346562-default-rtdb.firebaseio.com",
+    projectId: "messagestorage7346562",
+    storageBucket: "messagestorage7346562.appspot.com",
+    messagingSenderId: "858210728204",
+    appId: "1:858210728204:web:6bc76cd4b4c0c4153ef129"
+  };
 
 // Initialize Firebase (using legacy Firebase global object)
 firebase.initializeApp(firebaseConfig);
